d2fc253f4441016150b5aca2848c106e3635d3cc
uoflCoder <stcorn01@cardmail.louisville.edu>
2016-09-29 00:28:04 -0400

    Changed all the files appropriately texting should work and send to 5026815707

--- app/src/main/java/com/uoflsoftwareengineering/softwareteamproject/MainActivity.java
+++ app/src/main/java/com/uoflsoftwareengineering/softwareteamproject/MainActivity.java
@@ -1,13 +1,119 @@
 package com.uoflsoftwareengineering.softwareteamproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity
+{
+    Button btnSendSMS;
+    ImageButton btnSettings;
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSendSMS = (Button) findViewById(R.id.btnSendSMS);
+        btnSettings = (ImageButton) findViewById(R.id.imgBtnSettings);
+
+
+        btnSendSMS.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                String phoneNo = "5026815707";
+                String message = "This is a test";
+                if (phoneNo.length()>0 && message.length()>0)
+                    sendSMS(phoneNo, message);
+                else
+                    Toast.makeText(getBaseContext(),
+                            "Please enter both phone number and message.",
+                            Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent myIntent = new Intent(MainActivity.this,SettingsActivity.class
+                );
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+    }
+
+    private void sendSMS(String phoneNumber, String message)
+    {
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(SENT), 0);
+
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(DELIVERED), 0);
+
+        //---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS sent",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "Generic failure",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "No service",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(), "Null PDU",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+
+        //---when the SMS has been delivered---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), "SMS not delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(DELIVERED));
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
     }
 }
--- app/src/main/java/com/uoflsoftwareengineering/softwareteamproject/SettingsActivity.java
+++ app/src/main/java/com/uoflsoftwareengineering/softwareteamproject/SettingsActivity.java
@@ -1,13 +1,31 @@
 package com.uoflsoftwareengineering.softwareteamproject;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+
+/**
+ * Created by Tylor on 9/18/2016.
+ */
+public class SettingsActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        ImageButton btnSettings = (ImageButton) findViewById(R.id.imgBtnBack);
+        btnSettings.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(SettingsActivity.this,MainActivity.class);
+        startActivity(i);
     }
 }
